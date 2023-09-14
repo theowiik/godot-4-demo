8ec9e887d4bf99dceb23bed23447395484fd0457
theowiik <theo.le.wiik@gmail.com>
2023-09-14 15:56:47 +0200

    Add GoSharper

---
+++ 2D/GoSharper/Instancing/InstantiableAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace toybox.GoSharper.Instancing;
+
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class InstantiableAttribute : Attribute
+{
+  public InstantiableAttribute(string path)
+  {
+    Path = path;
+  }
+
+  public string Path { get; }
+}
--- 2D/Scripts/Spawner.cs
+++ 2D/Scripts/Spawner.cs
@@ -3,12 +3,11 @@ using System;
 
 public partial class Spawner : Node2D
 {
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        GD.Print("hello world");
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
     }
