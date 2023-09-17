2aa8d2929689ff32fdd6f1eb7a61345942fb5870
theowiik <42991003+theowiik@users.noreply.github.com>
2023-09-17 19:22:57 +0200

    Add ball texture

--- 2D/Scripts/World.cs
+++ 2D/Scripts/World.cs
@@ -1,4 +1,5 @@
 using Godot;
+using GodotSharper;
 using GodotSharper.AutoGetNode;
 
 namespace toybox.Scripts;
@@ -8,10 +9,22 @@ public partial class World : Node2D
     [GetNode("Spawner")]
     private Spawner _spawner;
 
+    [GetNode("Void")]
+    private Area2D _void;
+
     public override void _Ready()
     {
         this.GetNodes();
         _spawner.BallSpawned += OnBallSpawned;
+        _void.BodyEntered += OnVoidBodyEntered;
+
+        AddChild(TimerFactory.StartedOneShot(10, () => GD.Print("Hello World!")));
+    }
+
+    private static void OnVoidBodyEntered(Node2D body)
+    {
+        if (body is Ball ball)
+            ball.QueueFree();
     }
 
     private void OnBallSpawned(Node2D ball, Vector2 globalPosition)
